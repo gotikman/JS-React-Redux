283e50c42eed167014a11354bc4165383d800d5e
Rus <mail@test.com>
2021-08-31 21:13:49 +0300

    console comands

---
+++ JavaScript/!Practice/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const numberOfFilms =  +prompt("Сколько фильмов вы уже посмотрели?", "");
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+// console.log(personalMovieDB);
+
+const a = prompt("Один из последних просмотренных фильмов?",""),
+      b = +prompt("На сколько оцените его?",""),
+      c = prompt("Один из последних просмотренных фильмов?",""),
+      d = +prompt("На сколько оцените его?","");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+personalMovieDB.newfield = numberOfFilms;
+
+console.log(personalMovieDB);
+
+
+
