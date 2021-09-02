c2418fff8b7a4de762232bc0821fc0be1bb82509
gotikman <vydyvnyk@gmail.com>
2021-09-02 18:49:43 +0300

    add functions

---
+++ JavaScript/8.functions/js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage (text) {
+    console.log(text);
+    console.log(num);
+}
+
+showFirstMessage ('Йоу');
+console.log(num);
+// _____________________
+
+function calc( a, b ) {
+    return( a + b );
+}
+
+console.log(calc( 4 , 3 ));
+// console.log(calc( 55 , 44 ));
+// console.log(calc( 22 , 55 ));
+
+
+function ret() {
+        let num = 9999;
+
+        // ! код, може бути певна логіка
+
+        return num;      /* повертає значення лок змінної на вверх */
+        
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+// _____________________
+
+const logger = function(){
+    console.log('Hello');
+}
