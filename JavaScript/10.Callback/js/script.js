9b95e7ec9a4e9863c539c287d1cc3f9dd10b3953
gotikman <vydyvnyk@gmail.com>
2021-09-06 14:21:31 +0300

    callback

---
+++ JavaScript/10.Callback/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function first () {
+    // Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+// __________________
+
+function learnJS(lang, Callback) {
+    console.log(`я вчу: ${lang}`);
+    Callback();
+}
+
+learnJS('JavaScript', function() {
+    console.log('я пройшов цей урок');
+});
