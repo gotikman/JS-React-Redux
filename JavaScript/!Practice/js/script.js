14645bff84879734659dbabfd78eca7e383cda56
gotikman <vydyvnyk@gmail.com>
2021-09-10 14:05:26 +0300

    home w

--- JavaScript/!Practice/js/script.js
+++ JavaScript/!Practice/js/script.js
@@ -1,81 +1,100 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms =  +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    // ! цикл виконується поки одна з умов = true
-    // ! провіряєм на "Cansel", "Ok" і букви в рядку
-
-    while (numberOfFilms == ""  || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms =  +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: function () {
+        personalMovieDB.count =  +prompt("Сколько фильмов вы уже посмотрели?", "");
+    
+        // ! цикл виконується поки одна з умов = true
+        // ! провіряєм на "Cansel", "Ok" і букви в рядку
+    
+        while (personalMovieDB.count == ""  || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count =  +prompt("Сколько фильмов вы уже посмотрели?", "");
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+        
+            const a = prompt("Один из последних просмотренных фильмов?",''),
+                  b = prompt("Oцінка:?",'');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {    
+                personalMovieDB.movies[a] = b; 
+                console.log('Готово');
+            }
+        
+            else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if ( personalMovieDB.count < 10 ) {
+            console.log('Досить мало!'); 
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+                console.log('Середня к-ть');
+        } else if (personalMovieDB.count >= 30) {
+                console.log('Ти кіноман');
+        } else {
+                console.log('Відбулася помилка');
+        }    
+    },
+    showMyDb: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        } 
+    },
+    toggleVisibleMyDb: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    // writeYourGenres: function() {
+    //     for (let i = 1; i<=3; i++) {
+    //        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+    //         if (genre === '' || genre == null) {
+    //             console.log('Ви ввели не коректіні дані або не ввели взагалі');
+    //             i--;
+    //         } else {
+    //             // ! [i - 1] якщо записуєм в масив з 1-о місця, тобто 0
+    //             personalMovieDB.genres[i - 1] = genre;
+    //         }            
+    
+    //     }
+    //     personalMovieDB.genres.forEach((item, i) => {
+    //             console.log(`Любимый жанр ${i + 1} - это ${item}` );
+    //     });
+    // }
+    
+
+    //!  Альтернативний спосіб зі Split і Sort+toLowerCase   
+
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {
+        
+            let genres = prompt(`Введіть ваші улюблені жанри через кому ","`).toLowerCase();     
+
+            if (genres === '' || genres == null) {
+                console.log('Ви ввели не коректіні дані або не ввели взагалі');
+                        i--;
+             } else {                        
+                 personalMovieDB.genres = genres.split(', ');
+                 personalMovieDB.genres.sort();
+             }     
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+                console.log(`Любимый жанр ${i + 1} - это ${item}` );
+        });
+    }
 };
 
-
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-    
-        const a = prompt("Один из последних просмотренных фильмов?",''),
-              b = prompt("Oцінка:?",'');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {    
-            personalMovieDB.movies[a] = b; 
-            console.log('Готово');
-        }
-    
-        else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
-
-
-function detectPersonalLevel () {
-    if ( personalMovieDB.count < 10 ) {
-        console.log('Досить мало!'); 
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
-            console.log('Середня к-ть');
-    } else if (personalMovieDB.count >= 30) {
-            console.log('Ти кіноман');
-    } else {
-            console.log('Відбулася помилка');
-    }
-
-}
-
-detectPersonalLevel();
-
-function showMyDb (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    } 
-}
-
-showMyDb(personalMovieDB.privat);
-
-
-function writeYourGenres () {
-    for (let i = 1; i<=3; i++) {
-        // const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        // personalMovieDB.genres[i - 1] = genre;
-        // ! [i - 1] якщо записуєм в масив з 1-о місця, тобто 0
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-
-    }
-}
-writeYourGenres();
+ 
---
+++ JavaScript/14. OOP/readme.txt
@@ -0,0 +1,5 @@
+Object.prototype.__proto__
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
+
+Prototype methods, objects without __proto__
+https://javascript.info/prototype-methods
