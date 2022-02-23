a02b6fc993e0a973292d2e788c72facdc6b4be98
gotikman <vydyvnyk@gmail.com>
2022-02-23 23:24:03 +0200

    Routing GitHub Pages

--- React/marvel-api/src/components/app/App.js
+++ React/marvel-api/src/components/app/App.js
@@ -10,7 +10,6 @@ import Spinner from '../spinner/Spinner';
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-// const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
 const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
 const SinglePage = lazy(() => import('../pages/SinglePage'));
@@ -20,7 +19,7 @@ const SinglePage = lazy(() => import('../pages/SinglePage'));
 const App = () => {
 
     return (
-        <Router>
+        <Router >
             <div className="app">
                 <AppHeader />
                 <main>
@@ -28,10 +27,10 @@ const App = () => {
                     <Suspense fallback={<Spinner />}>
 
                         <Routes>
-                            <Route path="/" element={<MainPage />} />
-                            <Route path="/comics" element={<ComicsPage />} />
-                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
-                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character' />} />
+                            <Route path="/www-react-api" element={<MainPage />} />
+                            <Route path="/www-react-api/comics" element={<ComicsPage />} />
+                            <Route path="/www-react-api/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
+                            <Route path="/www-react-api/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character' />} />
                             <Route path="*" element={<Page404 />} />
                         </Routes>
 
@@ -44,3 +43,49 @@ const App = () => {
 }
 
 export default App;
+
+// import { lazy, Suspense } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// // import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+// import AppHeader from "../appHeader/AppHeader";
+// import Spinner from '../spinner/Spinner';
+
+// //? динамічні імпорти підкл. після статичних 
+// const Page404 = lazy(() => import('../pages/404'));
+// const MainPage = lazy(() => import('../pages/MainPage'));
+// const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+// // const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+// const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+// const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+// const SinglePage = lazy(() => import('../pages/SinglePage'));
+
+
+
+// const App = () => {
+
+//     return (
+//         <Router >
+//             <div className="app">
+//                 <AppHeader />
+//                 <main>
+
+//                     <Suspense fallback={<Spinner />}>
+
+//                         <Routes>
+//                             <Route path="/" element={<MainPage />} />
+//                             <Route path="/comics" element={<ComicsPage />} />
+//                             <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
+//                             <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character' />} />
+//                             <Route path="*" element={<Page404 />} />
+//                         </Routes>
+
+//                     </Suspense>
+
+//                 </main>
+//             </div>
+//         </Router >
+//     )
+// }
+
+// export default App;
