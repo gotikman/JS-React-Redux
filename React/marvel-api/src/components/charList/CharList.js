66784dd5bcf49ac038e3828ed45ffba3b800f4f2
gotikman <vydyvnyk@gmail.com>
2022-04-06 20:58:30 +0300

    finite-state machine

--- React/marvel-api/src/components/charList/CharList.js
+++ React/marvel-api/src/components/charList/CharList.js
@@ -5,7 +5,27 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMassage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
+
 import './charList.scss';
+
+const setContent = (process, Component, newItemLoading) => {
+    switch (process) {
+        case 'waiting':
+            return <Spinner />;
+        // break;
+        case 'loading':
+            return newItemLoading ? <Component /> : <Spinner />;
+        // break;
+        case 'confirmed':
+            return <Component />;
+        // break;
+        case 'error':
+            return <ErrorMessage />;
+        // break;
+        default:
+            throw new Error('Unexpected process state');
+    }
+}
 
 const CharList = (props) => {
 
@@ -14,7 +34,7 @@ const CharList = (props) => {
     const [offset, setOffset] = useState(210);                      // 1539 тестування останніх
     const [charEnded, setCharEnded] = useState(false);
 
-    const { loading, error, getAllCharacters } = useMarvelService();  //!
+    const { getAllCharacters, process, setProcess } = useMarvelService();  //!
 
     useEffect(() => {
         onRequest(offset, true);                          // викликаєм без значення, в сервісі підставиться дефолтний
@@ -25,6 +45,7 @@ const CharList = (props) => {
         initial ? setNewItemLoading(false) : setNewItemLoading(true);
         getAllCharacters(offset)
             .then(onCharListLoaded)
+            .then(() => setProcess('confirmed'));
     }
 
 
@@ -94,19 +115,10 @@ const CharList = (props) => {
 
     // __________________________________________________________________
 
-
-    const items = renderItems(charList);
-
-    const spinner = loading && !newItemLoading ? <Spinner /> : null;
-    const errorMassage = error ? <ErrorMessage /> : null;
-
-
-
     return (
         <div className="char__list">
-            {spinner}
-            {errorMassage}
-            {items}
+
+            {setContent(process, () => renderItems(charList), newItemLoading)}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
