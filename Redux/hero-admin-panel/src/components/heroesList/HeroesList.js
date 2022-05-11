878e56504226c86e36d697412058d463a13a27b7
gotikman <vydyvnyk@gmail.com>
2022-05-11 19:25:59 +0300

    combineReducers, createSelector

--- Redux/hero-admin-panel/src/components/heroesList/HeroesList.js
+++ Redux/hero-admin-panel/src/components/heroesList/HeroesList.js
@@ -1,12 +1,8 @@
-//* Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-//* Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
-
 import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { createSelector } from 'reselect'
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -15,7 +11,21 @@ import Spinner from '../spinner/Spinner';
 import './heroesList.scss';
 
 const HeroesList = () => {
-    const { filteredHeroes, heroesLoadingStatus } = useSelector(state => state);
+
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes;
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        }
+    );
+
+    const filteredHeroes = useSelector(filteredHeroesSelector)  //!
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
     const dispatch = useDispatch();
     const { request } = useHttp();
 
