79f2e0ebb64dd492e922862c7c3a4aae6409ce9a
gotikman <vydyvnyk@gmail.com>
2022-06-22 21:46:11 +0300

    mark, edit cover

--- JavaScript/07. Cycles/js/script.js
+++ JavaScript/07. Cycles/js/script.js
@@ -1,10 +1,58 @@
 "use strict";
 
- let num = 50;
+let num = 50;
 
- do {
-    console.log(num);
-    num++;
- }
- while (num<55);
+do {
+   console.log(num);
+   num++;
+}
+while (num < 55);
+
+
+//!  Cicle in Cicle
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// ----------------------------------
+
+let result = '';
+const lenght = 7;
+
+for (let i = 0; i < lenght; i++) {
+   for (let j = 0; j < i; j++) {
+      result += '*';
+   }
+
+   result += "\n";
+}
+
+console.log(result);
+
+//! --------------МІТКИ--------------------
+
+first: for (let i = 0; i < 5; i++) {
+   console.log(`first level: ${i}`);
+   for (let j = 0; j < 5; j++) {
+      console.log(`second level: ${j}`);
+      for (let k = 0; k < 5; k++) {
+         if (k === 2) continue first;
+         console.log(`third level: ${k}`);
+      }
+   }
+}
+first: for (let i = 0; i < 5; i++) {
+   console.log(`first level: ${i}`);
+   for (let j = 0; j < 5; j++) {
+      console.log(`second level: ${j}`);
+      for (let k = 0; k < 5; k++) {
+         if (k === 2) break first;
+         console.log(`third level: ${k}`);
+      }
+   }
+}
 
