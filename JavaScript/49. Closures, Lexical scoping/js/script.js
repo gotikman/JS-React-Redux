5bd0049d0c9afd6addedab4e5cc585be09f935d5
gotikman <vydyvnyk@gmail.com>
2022-09-01 15:55:52 +0300

    closures

--- JavaScript/49. Closures, Lexical scoping/js/script.js
+++ JavaScript/49. Closures, Lexical scoping/js/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+// let number = 5; debugger;
+
+// function logNumber() {
+//     console.log(number); debugger
+// }
+
+// number = 6;
+
+// logNumber(); debugger;
+
+//! Замикання -  кожен виклик функції створює нове ликсичене оточення зі своїми змінними та особливостями
+
+function createCounter() { // fn що повертає іншу fn
+    let counter = 0;
+
+    const myFunction = function () {
+        counter = counter + 1; debugger
+        return counter; debugger
+    }
+
+    return myFunction;
+}
+
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
+
+console.log(c1, c2, c3)
+
+// ____________________________________
+
+for (let i = o; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        let num = 3
+    }
+    // error томущо num в іншому лексичному оточені
+    console.log(num)
+}
--- JavaScript/49. Closures, Lexical scoping/readme.txt
+++ JavaScript/49. Closures, Lexical scoping/readme.txt
@@ -1,2 +1,13 @@
 = Udemy lason =
 https://www.udemy.com/course/javascript_full/learn/lecture/31204818
+
+Замыкание
+https://learn.javascript.ru/closure
+
+Я никогда не понимал замыкания в JavaScript
+https://medium.com/nuances-of-programming/%D1%8F-%D0%BD%D0%B8%D0%BA%D0%BE%D0%B3%D0%B4%D0%B0-%D0%BD%D0%B5-%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%BB-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-%D1%87%D0%B0%D1%81%D1%82%D1%8C-%D0%BF%D0%B5%D1%80%D0%B2%D0%B0%D1%8F-3c3f02041970
+
+
+Объект функции, NFE, Cвойства функции
+https://learn.javascript.ru/function-object
+
