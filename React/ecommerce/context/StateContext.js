ee0389fc1b995f6da6101c46893ee3f49c928de3
gotikman <vydyvnyk@gmail.com>
2022-11-25 19:54:13 +0200

    create Cart, stripe

--- React/ecommerce/context/StateContext.js
+++ React/ecommerce/context/StateContext.js
@@ -10,8 +10,8 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
-    // let foundProduct;
-    // let index;
+    let foundProduct;
+    let index;
 
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
